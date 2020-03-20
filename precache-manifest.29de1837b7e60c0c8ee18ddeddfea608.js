b2234c33b4f0b33d003be90c378be6ab315b64f3
Sivasankar <sivasankar.d@sap.com>
2020-03-20 17:20:16 +0530

    Updates

---
+++ precache-manifest.29de1837b7e60c0c8ee18ddeddfea608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edb9f0d751410004170283841720912",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8388251109a363da9403",
+    "url": "./static/css/main.e313a809.chunk.css"
+  },
+  {
+    "revision": "b479a51cc3a1c39cce27",
+    "url": "./static/js/2.da72784e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.da72784e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8388251109a363da9403",
+    "url": "./static/js/main.5a146c8f.chunk.js"
+  },
+  {
+    "revision": "fe70811e3aca94f54054",
+    "url": "./static/js/runtime-main.a455c9cf.js"
+  }
+]);
