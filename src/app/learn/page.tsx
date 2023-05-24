6feefacc584b128afc808d42e256e522105368e1
Siva <notgonnatellyou@hehe.com>
2023-05-24 00:04:53 -0700

    Fix logo, current page

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,3 +1,10 @@
 export default function Learn() {
-  return <div>content</div>;
+  return (
+    <div className="w-full text-center pt-24">
+      <p className="text-xl">
+        Sorry! This section of the website is still work in progress. Please
+        comeback later.
+      </p>
+    </div>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import classnames from "classnames";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <div className="border-b py-4 px-2 lg:px-0">
       <nav role="navigation" className="mx-auto lg:w-4/5 2xl:w-3/5">
         <ul className="sticky flex flex-row justify-between">
           <li>
             <div>
-              <Link href={"/"}>
+              <Link href={"/"} className="inline-flex gap-2">
+                <Image
+                  src={"/brand-32x32.png"}
+                  alt="Write assit logo"
+                  width={16}
+                  height={16}
+                  className=""
+                />
+                {"  "}
                 <span className="font-semibold">Write Assist</span>
               </Link>
             </div>
@@ -15,13 +31,25 @@ export default function Header() {
 
           <li>
             <ul className="flex flex-row gap-4">
-              <li>
+              <li
+                className={classnames({
+                  underline: pathname === "/",
+                })}
+              >
                 <Link href={"/"}>Home</Link>
               </li>
-              <li>
+              <li
+                className={classnames({
+                  underline: pathname === "/write",
+                })}
+              >
                 <Link href={"/write"}>Write</Link>
               </li>
-              <li>
+              <li
+                className={classnames({
+                  underline: pathname === "/learn",
+                })}
+              >
                 <Link href={"/learn"}>Learn</Link>
               </li>
             </ul>
